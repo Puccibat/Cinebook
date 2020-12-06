601f9f53eb88e39bc40f016d4bd33d20978fc84b
Baptiste <baptiste.puccinelli@gmail.com>
2020-12-06 08:13:50 +0100

    Hash password Done

--- controller/auth.js
+++ controller/auth.js
@@ -1,4 +1,5 @@
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { signupValidation } = require('../validation');
 
 signup = async (req, res) => {
@@ -6,7 +7,21 @@ signup = async (req, res) => {
   const { error } = signupValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = new User(req.body);
+  //Check if the user is already in the database
+  const checkedEmail = await User.findOne({ email: req.body.email });
+  if (checkedEmail)
+    return res.status(400).send('Email already exists, please go to signin');
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  //Create a new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
   try {
     const savedUser = await user.save();
     res.send(savedUser);
